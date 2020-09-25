ba1ddc269e713a809e9dbff9e7b714afe3c95f54
Anrower <anrowerdd@gmail.com>
2020-09-25 20:29:39 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let str = n.toString();
+        return str.split("").reverse().join("");
+    } else {
+        n *= -1;
+        let str = n.toString();
+        return str.split("").reverse().join("");
+    }
 }
